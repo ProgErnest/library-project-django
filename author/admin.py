730c71f4407cd5414d1d2a2aa6b4e789f194de4a
ProgErnest <cuba7843@gmail.com>
2026-07-31 18:04:51 +0100

    adding admin filters and parameters closes #18

--- author/admin.py
+++ author/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Author
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'birthday', 'nationality')
+    search_fields = ('name', 'surname')
+    list_filter = ('nationality', 'birthday')
+    ordering = ('name', 'surname')
+    list_per_page = 10
 
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
--- book/admin.py
+++ book/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'isbn', 'language', 'genre', 'num_pages', 'publication_date', 'available', 'author', 'summary', 'total_copies', 'available_copies')
+    search_fields = ('title', 'subtitle', 'isbn', 'author__name', 'author__surname')
+    
 admin.site.register(Book)
