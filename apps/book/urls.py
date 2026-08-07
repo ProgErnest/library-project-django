a16fb248c3b201182fa787a0b793baa795610e20
ProgErnest <cuba7843@gmail.com>
2026-08-07 06:11:20 +0100

    Update URLS

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('all/', views.BookListView.as_view(),name="get_all_books"),
-    path('detail/<int:pk>/', views.BookDetailView.as_view(),name="book_detail"),
+    # Books
+    path('', views.BookListView.as_view(),name="get_all_books"),
+    path('<int:pk>/', views.BookDetailView.as_view(),name="book_detail"),
     path('create/', views.BookCreateView.as_view(), name="create_book"),
-    path('update/<int:pk>/', views.BookUpdateView.as_view(), name="update_book"),
-    path('delete/<int:pk>/', views.BookDeleteView.as_view(), name="delete_book"),
+    path('<int:pk>/update/', views.BookUpdateView.as_view(), name="update_book"),
+    path('<int:pk>/delete/', views.BookDeleteView.as_view(), name="delete_book"),
 
+    # Genres
+    path('genres/', views.GenreListView.as_view(), name="get_all_genres"),
+    path('genres/<slug:slug>/', views.GenreDetailView.as_view(), name="genre_detail"),
+    path('genres/create/', views.GenreCreateView.as_view(), name="create_genre")
 ]
