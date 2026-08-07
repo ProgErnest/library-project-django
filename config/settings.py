69f4846e3d8416ee9db7f5c92035bee9f4380065
ProgErnest <cuba7843@gmail.com>
2026-08-07 06:44:28 +0100

    Configurations for uploads medias closes #48

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 load_dotenv()
 
 
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
 ]
 handler404 = "apps.author.views.page_non_trouvee"
 handler500 = "apps.author.views.erreur_serveur"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
