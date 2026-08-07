f3569d4a8f3feea11f64c84f8b8c5e1b06ba5cf1
ProgErnest <cuba7843@gmail.com>
2026-08-07 06:38:33 +0100

    closes #47

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # Books
+    # Books 
     path('', views.BookListView.as_view(),name="get_all_books"),
     path('<int:pk>/', views.BookDetailView.as_view(),name="book_detail"),
     path('create/', views.BookCreateView.as_view(), name="create_book"),
     path('<int:pk>/update/', views.BookUpdateView.as_view(), name="update_book"),
     path('<int:pk>/delete/', views.BookDeleteView.as_view(), name="delete_book"),
 
-    # Genres
+    # Genres 
     path('genres/', views.GenreListView.as_view(), name="get_all_genres"),
     path('genres/<slug:slug>/', views.GenreDetailView.as_view(), name="genre_detail"),
     path('genres/create/', views.GenreCreateView.as_view(), name="create_genre")
