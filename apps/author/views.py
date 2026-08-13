a50506cd68f1b479090596c0e417775d052874cf
ProgErnest <cuba7843@gmail.com>
2026-08-13 04:49:24 +0100

    Chnaged 403 to 404 in prod

--- apps/author/views.py
+++ apps/author/views.py
@@ -82,8 +82,8 @@ class AuthorDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
 
 
 def page_non_trouvee(request, exception):
-    return render(request, "404.html", status=404)
+    return render(request, "errors/404.html", status=404)
 
 
 def erreur_serveur(request):
-    return render(request, "500.html", status=500)
+    return render(request, "errors/500.html", status=500)
